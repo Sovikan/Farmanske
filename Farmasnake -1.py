c66c10969434220abbba4ca99534bb449cc2dfa3
Sovikan <165888386+Sovikan@users.noreply.github.com>
2024-04-03 10:23:19 +0200

    Add files via upload

---
+++ Farmasnake -1.py
@@ -0,0 +1,6 @@
+nombre = int(input("Entrez un nombre entre 0 et 255 : "))
+
+if 0 <= nombre <= 255:
+    print("Correct")
+else:
+    print("Incorrect")
---
+++ Farmasnake 1.py
@@ -0,0 +1,2 @@
+nombre = input("Entrez un nombre : ")
+print("Le nombre que vous avez entré est :", nombre)
